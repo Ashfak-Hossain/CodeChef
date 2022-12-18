64a5c14c3a91a56ef19dda9315aacfe30d69b731
Berlin <evan1234.ek@gmail.com>
2022-12-18 19:11:53 +0600

    solved

---
+++ Reverse_The_Number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int cases;
+    cin >> cases;
+
+    while (cases--)
+    {
+        int n;
+        cin >> n;
+        int ans = 0;
+        while (n)
+        {
+            ans = ans * 10 + n % 10;
+            n /= 10;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
