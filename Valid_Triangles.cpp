0feb87914c7a26a331237b20f0a8f7305a072635
Berlin <evan1234.ek@gmail.com>
2022-12-18 18:56:58 +0600

    solved

---
+++ Valid_Triangles.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int cases;
+    cin >> cases;
+
+    while (cases--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a + b + c <= 180)
+            cout << "YES" << endl;
+    }
+
+    return 0;
+}
