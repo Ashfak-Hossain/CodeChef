158230caed529b657d985a9f528a42c5c7372bee
Berlin <evan1234.ek@gmail.com>
2022-12-19 14:24:48 +0600

    solved

---
+++ Rain_in_Chefland.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int cases;
+    cin >> cases;
+
+    while (cases--)
+    {
+        int rateOfRainfall;
+        cin >> rateOfRainfall;
+
+        if (rateOfRainfall < 3)
+            cout << "LIGHT" << endl;
+        else if ((rateOfRainfall >= 3 && rateOfRainfall < 7))
+            cout << "MODERATE" << endl;
+        else
+            cout << "HEAVY" << endl;
+    }
+
+    return 0;
+}
