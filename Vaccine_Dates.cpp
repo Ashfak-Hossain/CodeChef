e27e0ca81913d9b65ec39702f823cea39c7b2986
Berlin <evan1234.ek@gmail.com>
2022-12-19 12:59:27 +0600

    Solved

---
+++ Vaccine_Dates.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int cases;
+    cin >> cases;
+
+    while (cases--)
+    {
+        int d, l, r;
+
+        cin >> d >> l >> r;
+
+        if (d < l)
+            cout << "Too Early" << endl;
+        else if (d > r)
+            cout << "Too late" << endl;
+        else if ((d >= l) && (d <= r))
+            cout << "Take second dose now" << endl;
+    }
+
+    return 0;
+}
